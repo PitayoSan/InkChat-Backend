cc17e4bd5b014d468d232d6eef6fa0596b90bc45
0miguel <miguelbarroe@gmail.com>
2021-04-14 14:56:18 -0500

    key

--- flask_app.py
+++ flask_app.py
@@ -4,12 +4,10 @@ from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
 
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-  'projectId': 'inkchat-58958',
-})
+cred = credentials.Certificate("./key.json")
+firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 @app.route('/')
 def hello_world():
-    return 'Hello from InkChat!'
+	return 'Hello from InkChat!'
