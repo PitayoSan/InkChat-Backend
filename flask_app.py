a1683a745706676f29b2e0e7a67ab2c9aec255da
0miguel <miguelbarroe@gmail.com>
2021-04-14 14:52:11 -0500

    ignore

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
+
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'inkchat-58958',
+})
+db = firestore.client()
 
 @app.route('/')
 def hello_world():
