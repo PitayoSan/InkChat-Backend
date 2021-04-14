bb88ee62237587cdbd61d2d655aa33dda3337f79
0miguel <miguelbarroe@gmail.com>
2021-04-14 15:25:26 -0500

    read and create/update

--- flask_app.py
+++ flask_app.py
@@ -8,6 +8,20 @@ cred = credentials.Certificate("./key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# create/update
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+
+# read
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
+
 @app.route('/')
 def hello_world():
 	return 'Hello from InkChat!'
