a77ebe4a99fe7dff4d3761da2ab12dc71771ba5b
PitayoSan <carloslopezels@gmail.com>
2021-05-04 13:04:11 -0500

    Add 'home' test
    Signed-off-by: PitayoSan <carloslopezels@gmail.com>

---
+++ tests/functional/test_home.py
@@ -0,0 +1,12 @@
+import pytest
+
+from flaskr import app
+
+
+def test_home(client):
+    response = client.get('/')
+    print(response)
+    json_data = response.get_json()
+
+    assert json_data["data"] == "This is the InkChat server"
+        
